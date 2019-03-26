242c658fc527a7fded5576ea34569eeaba77318a
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-26 14:18:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
 def my_collect(array)
   i = 0 
-  while i > array.length
-  yield array [1]
-  i += 1 
   new_array = []
-  new_array << "#{array}"
+  while i > array.length
+  new_array << yield array [1]
+  i += 1 
 end 
 new_array
 end 
